58c31f9390b0cadfb4f0daecfee8ed5e970ea055
Daniel <danielscheer@live.nl>
2024-10-14 17:21:48 +0200

    add options

--- lua/memento/init.lua
+++ lua/memento/init.lua
@@ -1,43 +1,74 @@
 local Memento = {}
+Memento.options = {
+    bufnr = nil,
+    tabpages = {},
+    width = 30,
+    side = "left",
+    winopts = {
+        relativenumber = false,
+        number = false,
+        list = false,
+        winfixwidth = true,
+        winfixheight = true,
+        foldenable = false,
+        spell = false,
+        signcolumn = "yes",
+        foldmethod = "manual",
+        foldcolumn = "0",
+        cursorcolumn = false,
+        colorcolumn = "0",
+    },
+    bufopts = {
+        { name = "swapfile", val = false },
+        { name = "buftype", val = "nofile" },
+        { name = "modifiable", val = false },
+        { name = "filetype", val = "SidebarNvim" },
+        { name = "bufhidden", val = "hide" },
+    },
+}
 
 -- Function to create the notepad window
 function Memento.create_window()
-  local width = 30
-  local height = 20
+    local width = Memento.options.width
+    local height = 20
 
-  -- Create a new split window on the left
-  vim.cmd('vsplit')
-  vim.cmd('resize ' .. height)
-  vim.cmd('setlocal buftype=nofile')
-  vim.cmd('setlocal bufhidden=wipe')
-  vim.cmd('setlocal noswapfile')
-  
-  -- Set the buffer name
-  vim.api.nvim_buf_set_name(0, "Memento")
+    -- Create a new split window on the left
+    vim.cmd('vsplit')
+    vim.cmd('resize ' .. height)
+    
+    -- Set window options
+    for key, value in pairs(Memento.options.winopts) do
+        vim.wo[key] = value
+    end
 
-  -- Set options for the window
-  vim.wo.number = true
-  vim.wo.relativenumber = true
-  vim.wo.foldmethod = 'expr'
-  vim.wo.foldexpr = '0'
+    -- Set buffer options
+    vim.api.nvim_buf_set_name(0, "Memento")
+    for _, opt in ipairs(Memento.options.bufopts) do
+        vim.api.nvim_buf_set_option(0, opt.name, opt.val)
+    end
 
-  -- Optionally set some initial content
-  vim.api.nvim_buf_set_lines(0, 0, -1, false, {"# Your notes go here..."})
+    -- Optionally set some initial content
+    vim.api.nvim_buf_set_lines(0, 0, -1, false, {"# Your notes go here..."})
 end
 
 -- Command to toggle the notepad window
 function Memento.toggle()
-  if vim.fn.bufexists('Memento') == 1 then
-    vim.cmd('b Memento')
-    vim.cmd('wincmd p')  -- Go back to the previous window
-  else
-    Memento.create_window()
-  end
+    if vim.fn.bufexists('Memento') == 1 then
+        vim.cmd('b Memento')
+        vim.cmd('wincmd p')  -- Go back to the previous window
+    else
+        Memento.create_window()
+    end
 end
 
--- Create the user command within the module
-function Memento.setup()
-  vim.api.nvim_create_user_command('ToggleMemento', Memento.toggle, {})
+-- Create the user command and setup options
+function Memento.setup(user_options)
+    if user_options then
+        for key, value in pairs(user_options) do
+            Memento.options[key] = value
+        end
+    end
+    vim.api.nvim_create_user_command('ToggleMemento', Memento.toggle, {})
 end
 
 return Memento
